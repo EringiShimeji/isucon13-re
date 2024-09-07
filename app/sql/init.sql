4794a0cfd63bcb2f1f65bd8270f4b4de677e1647
Sota Matsuo <59910028+EringiShimeji@users.noreply.github.com>
2024-09-07 12:56:10 +0900

    pdns用のindexを追加

--- app/sql/init.sql
+++ app/sql/init.sql
@@ -10,3 +10,7 @@ ALTER TABLE `tags` auto_increment = 1;
 ALTER TABLE `livecomments` auto_increment = 1;
 ALTER TABLE `livestreams` auto_increment = 1;
 ALTER TABLE `users` auto_increment = 1;
+
+USE `isudns`;
+ALTER TABLE `records` ADD INDEX disabled_name_domain_id_idx(`disabled`, `name`, `domain_id`);
+ALTER TABLE `records` ADD INDEX disabled_name_domain_id_idx(`disabled`, `type`, `name`);

8e4b6e8c5c3c69c8192d3ce0d24779e58eba4e19
Sota Matsuo <59910028+EringiShimeji@users.noreply.github.com>
2024-09-08 17:12:32 +0900

    signal対応

--- app/go/main.go
+++ app/go/main.go
@@ -11,7 +11,9 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
 	"strconv"
+	"syscall"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -251,6 +253,15 @@ func main() {
 	}
 	e.Listener = listener
 	defer func() {
+		err := listener.Close()
+		if err != nil {
+			log.Fatalf("Failed to close listener: %s.", err)
+		}
+	}()
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
 		err := listener.Close()
 		if err != nil {
 			log.Fatalf("Failed to close listener: %s.", err)

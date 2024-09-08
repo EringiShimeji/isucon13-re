c16674b8b73816da7c57cf23b4dd1719cdaaaa4e
Sota Matsuo <59910028+EringiShimeji@users.noreply.github.com>
2024-09-08 15:48:04 +0900

    fillLivecommentResponseにuserのキャッシュを導入

--- app/go/livecomment_handler.go
+++ app/go/livecomment_handler.go
@@ -409,8 +409,12 @@ func moderateHandler(c echo.Context) error {
 }
 
 func fillLivecommentResponse(ctx context.Context, tx *sqlx.Tx, livecommentModel LivecommentModel) (Livecomment, error) {
-	commentOwnerModel := UserModel{}
-	if err := tx.GetContext(ctx, &commentOwnerModel, "SELECT * FROM users WHERE id = ?", livecommentModel.UserID); err != nil {
+	commentOwnerModel, err := getOrInsertMap(&cache.user, livecommentModel.UserID, func() (UserModel, error) {
+		m := UserModel{}
+		err := tx.GetContext(ctx, &m, "SELECT * FROM users WHERE id = ?", livecommentModel.UserID)
+		return m, err
+	})
+	if err != nil {
 		return Livecomment{}, err
 	}
 	commentOwner, err := fillUserResponse(ctx, tx, commentOwnerModel)
